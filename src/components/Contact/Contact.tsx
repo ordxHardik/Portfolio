ec46f30496d87bea6b65d3e2f01c7af5b767d75a
Hardik Jain <jainhardik750@gmail.com>
2024-11-21 22:51:44 +0530

    contacts detail changed

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,29 +1,29 @@
-import { Container } from "./styles";
-import emailIcon from "../../assets/email-icon.svg";
-import phoneIcon from "../../assets/phone-icon.svg"
-import { Form } from "../Form/Form";
+// import { Container } from "./styles";
+// import emailIcon from "../../assets/email-icon.svg";
+// import phoneIcon from "../../assets/phone-icon.svg"
+// import { Form } from "../Form/Form";
 
 
-export function Contact(){
+// export function Contact(){
 
-  return(
-    <Container id="contact">
-      <header>
-        <h2>Contact</h2>
-        <p>Ready to get started on your project? </p>
-        <p>Contact me now for a Free consultation.</p>
-      </header>
-      <div className="contacts">
-        <div>
-        <a href="mailto:jainhardik750@gmail.com"><img src={emailIcon} alt="Email" /></a> 
-          <a href="mailto:jainhardik750@gmail.com">jainhardik750@gmail.com</a>
-        </div>
-        <div>
-        <a href="tel:+919996988819"><img src={phoneIcon} alt="Phone No" /></a>
-          <a href="tel:+919996988819">(+91) 9996988819</a>
-        </div>  
-      </div>
-      <Form></Form>
-    </Container>
-  )
-}
+//   return(
+//     <Container id="contact">
+//       <header>
+//         <h2>Contact</h2>
+//         <p>Ready to get started on your project? </p>
+//         <p>Contact me now for a Free consultation.</p>
+//       </header>
+//       <div className="contacts">
+//         <div>
+//         <a href="mailto:jainhardik750@gmail.com"><img src={emailIcon} alt="Email" /></a> 
+//           <a href="mailto:jainhardik750@gmail.com">jainhardik750@gmail.com</a>
+//         </div>
+//         <div>
+//         <a href="tel:+919996988819"><img src={phoneIcon} alt="Phone No" /></a>
+//           <a href="tel:+919996988819">(+91) 9996988819</a>
+//         </div>  
+//       </div>
+//       <Form></Form>
+//     </Container>
+//   )
+// }
